6c28ae025c2123d044d566c3f761d562821ff17a
Jakub Grobelny <jgrobelny1@gmail.com>
2020-01-17 01:35:30 +0100

    Basic window drawing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+extern crate sdl2;
+
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use std::time::Duration;
+
+const FPS: u32 = 60;
+
+fn main() -> Result<(), String> {
+    let context = sdl2::init()?;
+    let video = context.video()?;
+
+    let window = video
+        .window("mario game", 640, 480)
+        .position_centered()
+        .resizable()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
+
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+    canvas.clear();
+    canvas.present();
+    let mut event_pump = context.event_pump()?;
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
+                    break 'running;
+                }
+                _ => (),
+            }
+        }
+
+        canvas.clear();
+        canvas.present();
+
+        std::thread::sleep(Duration::new(0, 1_000_000_000u32 / FPS));
+    }
+
+    Ok(())
 }
